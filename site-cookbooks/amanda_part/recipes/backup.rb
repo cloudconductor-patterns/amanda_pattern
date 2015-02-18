1ad89796dcf37db5a52f0cbc4c36f26fb7c5eb1d
b03b88c9a1df <b03b88c9a1df@gmail.com>
2015-02-18 07:47:48 +0000

    Refactor backup recipe.

--- site-cookbooks/amanda_part/recipes/backup.rb
+++ site-cookbooks/amanda_part/recipes/backup.rb
@@ -1,4 +1,10 @@
-execute 'amdump' do
-  user node['amanda_part']['execuser']
-  command "amdump #{node['amanda_part']['config_name']}"
+host_backup_restore_config.each do |hostname, backup_restore_config|
+  backup_restore_config.each do |path_config|
+    config = amanda_config(hostname, path_config[:path])
+    execute "amdump_#{config['name']}" do
+      user node['amanda_part']['execuser']
+      command "amdump #{config['name']}"
+      only_if { server? }
+    end
+  end
 end
