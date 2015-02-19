e9ca4b734b4d743a379d0770dc3103a124ace659
b03b88c9a1df <b03b88c9a1df@gmail.com>
2015-02-19 03:27:32 +0000

    Add restore recipe.

---
+++ site-cookbooks/amanda_part/recipes/restore.rb
@@ -0,0 +1,17 @@
+hostname = `hostname`.strip
+host_backup_restore_config[hostname].each do |path_config|
+  config = amanda_config(hostname, path_config[:path])
+  bash "amrecover_#{config['name']}" do
+  code <<-EOH
+    amrecover -C #{config['name']} <<EOF
+      setdisk #{path_config[:path]}
+      lcd #{path_config[:path]}
+      add .
+      extract
+      Y
+      Y
+      exit
+    EOF
+  EOH
+  end
+end
