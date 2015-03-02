52c6973d3a090b34377d36c9434fc649bc30e810
b03b88c9a1df <b03b88c9a1df@gmail.com>
2015-03-02 02:28:48 +0000

    Fix errors.

--- site-cookbooks/amanda_part/recipes/backup.rb
+++ site-cookbooks/amanda_part/recipes/backup.rb
@@ -1,4 +1,4 @@
-host_backup_restore_config.each do |hostname, backup_restore_config|
+host_config.each do |hostname, backup_restore_config|
   backup_restore_config.each do |path_config|
     config = amanda_config(hostname, path_config[:path])
     execute "amdump_#{config[:name]}" do
