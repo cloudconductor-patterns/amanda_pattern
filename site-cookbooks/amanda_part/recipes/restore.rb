9a71f63069c01092842f31cfc53c7044737156f7
b03b88c9a1df <b03b88c9a1df@gmail.com>
2015-03-04 01:48:15 +0000

    Fix restore rule.

--- site-cookbooks/amanda_part/recipes/restore.rb
+++ site-cookbooks/amanda_part/recipes/restore.rb
@@ -1,28 +1,31 @@
-host_config[current_hostname][:paths].each do |path_config|
-  config = amanda_config(current_hostname, path_config[:path])
-  ruby_block "amrecover_#{config[:name]}" do
-    block do
-      require 'pty'
-      require 'expect'
-      sequence = [
-        "setdisk #{path_config[:path]}",
-        "lcd #{path_config[:path]}",
-        'add *',
-        'extract',
-        'quit'
-      ]
-      amrecover = ['amrecover', '-C', "#{config[:name]}"].join(' ')
-      PTY.getpty(amrecover) do |reader, writer, _pid|
-        writer.sync = true
-        until sequence.empty? || reader.eof?
-          reader.expect(/(>|\?) $/, 60) do |match|
-            break unless match
-            case match[1]
-            when />/
-              command = sequence.shift
-              writer.puts command
-            when /\?/
-              writer.puts 'Y'
+roles = ENV['ROLE'].split(',')
+roles.each do |role|
+  host_config[role.to_sym][:paths].each do |path_config|
+    config = amanda_config(role, path_config[:path])
+    ruby_block "amrecover_#{config[:name]}" do
+      block do
+        require 'pty'
+        require 'expect'
+        sequence = [
+          "setdisk #{path_config[:path]}",
+          "lcd #{path_config[:path]}",
+          'add *',
+          'extract',
+          'quit'
+        ]
+        amrecover = ['amrecover', '-C', "#{config[:name]}"].join(' ')
+        PTY.getpty(amrecover) do |reader, writer, _pid|
+          writer.sync = true
+          until sequence.empty? || reader.eof?
+            reader.expect(/(>|\?) $/, 60) do |match|
+              break unless match
+              case match[1]
+              when />/
+                command = sequence.shift
+                writer.puts command
+              when /\?/
+                writer.puts 'Y'
+              end
             end
           end
         end
