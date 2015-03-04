a3f1edd0176dc9740fb79e65b22e75bbe165906a
b03b88c9a1df <b03b88c9a1df@gmail.com>
2015-03-04 01:40:19 +0000

    Fix backup rule.

--- site-cookbooks/amanda_part/recipes/backup.rb
+++ site-cookbooks/amanda_part/recipes/backup.rb
@@ -1,9 +1,9 @@
-host_config.each do |hostname, backup_restore_config|
-  backup_restore_config[:paths].each do |path_config|
-    config = amanda_config(hostname, path_config[:path])
+role_host_config.each do |role, role_host_backup_restore_config|
+  role_host_backup_restore_config[:paths].each do |path_config| 
+    config = amanda_config(role, path_config[:path])
     execute "amdump_#{config[:name]}" do
       command "su - #{node['amanda_part']['user']} -c \"amdump #{config[:name]}\""
-      only_if { server? }
+      only_if { amanda_server? }
     end
   end
 end
