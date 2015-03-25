a8291f23bae64575d61c171f5272fe01bfdae06d
b03b88c9a1df <b03b88c9a1df@gmail.com>
2015-03-25 08:18:53 +0000

    Fix logic to skip first restore.

--- site-cookbooks/amanda_part/recipes/restore_s3.rb
+++ site-cookbooks/amanda_part/recipes/restore_s3.rb
@@ -3,13 +3,21 @@ Chef::Recipe.send(:include, CloudConductor::AmandaPartHelper)
 
 require 'aws-sdk-core'
 
+bucket_name = node['amanda_part']['server']['s3']['tpchanger']['bucket_name']
+s3 = Aws::S3::Client.new(
+  region: node['amanda_part']['server']['s3']['tpchanger']['s3_bucket_location'],
+  access_key_id: node['amanda_part']['server']['s3']['tpchanger']['s3_access_key'],
+  secret_access_key: node['amanda_part']['server']['s3']['tpchanger']['s3_secret_key']
+)
+
 parameters = CloudConductorUtils::Consul.read_parameters[:cloudconductor]
 roles = ENV['ROLE'].split(',')
+
 hosts_paths_privileges_by_role(roles, parameters).each do |role, role_config|
   role_config[:paths].each do |path_config|
     config = amanda_config(role, path_config[:path])
     restore_file = File.join(node['amanda_part']['amanda_restore_work_dir'], 'restore.tar')
-    next unless path_config[:restore_enabled]
+    next unless (path_config[:restore_enabled] && s3.list_objects(bucket: bucket_name).length > 0)
     execute 'execute pre_restore script' do
       command "su - #{node['amanda_part']['user']} -l -c /usr/libexec/amanda/application/pre_restore_#{config[:name]}"
       action :run
@@ -17,12 +25,6 @@ hosts_paths_privileges_by_role(roles, parameters).each do |role, role_config|
     end
     ruby_block 'download restore file' do
       block do
-        bucket_name = node['amanda_part']['server']['s3']['tpchanger']['bucket_name']
-        s3 = Aws::S3::Client.new(
-          region: node['amanda_part']['server']['s3']['tpchanger']['s3_bucket_location'],
-          access_key_id: node['amanda_part']['server']['s3']['tpchanger']['s3_access_key'],
-          secret_access_key: node['amanda_part']['server']['s3']['tpchanger']['s3_secret_key']
-        )
         objects = s3.list_objects(bucket: bucket_name)
         target_objects = objects.contents.select do |object|
           object.key.start_with?("#{role}/#{config[:disk_postfix]}/")
