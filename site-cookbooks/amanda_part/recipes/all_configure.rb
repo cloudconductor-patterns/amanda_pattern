5efe582e65beb9417aa11c8355a7b2670852655e
maruoka.takashi <maruoka.takashi@tis.co.jp>
2015-07-27 11:40:57 +0900

    Fix foodcritic FC048

--- site-cookbooks/amanda_part/recipes/all_configure.rb
+++ site-cookbooks/amanda_part/recipes/all_configure.rb
@@ -22,6 +22,7 @@ end
 
 ruby_block 'backup_restore_backup_ready_event' do
   block do
-    `consul event -name="backup_ready" "${CONSUL_SECRET_KEY}"`
+    cmd = Mixlib::ShellOut.new("consul event -name=\"backup_ready\" ${CONSUL_SECRET_KEY}")
+    cmd.run_command
   end
 end
